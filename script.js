47f46e2b1e62a7a0187673bf93b8f9425fa3729a
miwanoff <marina.iva.noffa@gmail.com>
2026-03-01 13:22:23 +0200

    Add Weather

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 "use strict";
 const weatherBlock = document.getElementById("weather");
-let city = "Kharkiv"
-let openweathermapURL =
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${openweathermapAPI}`;
+const errorBlock = document.getElementById("error");
+let city = "Kharkiv";
+let openweathermapURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${openweathermapAPI}`;
 
 console.log(openweathermapURL);
+
+async function loadWeather() {
+  weatherBlock.innerHTML = `<div class="weather_load"><img src="./images/w.png" alt="" class="loader"></div>`;
+  const response = await fetch(openweathermapURL, { method: "GET" });
+  const responseResult = await response.json();
+  if (response.ok) {
+    getWeather(responseResult);
+  } else {
+    errorBlock.innerHTML = responseResult.message;
+  }
+}
+
+function getWeather(data) {
+  console.log(data);
+  const city = data.name;
+  const temp = data.main.temp;
+  const icon = data.weather[0].icon
+  const template = `<div id="city">
+        <h2>${city}</h2>
+    </div>
+    <div id="temp">${temp}</div>
+    <div id="icon"><img src="https://api.openweathermap.org/img/w/${icon}.png" alt=""></div>`;
+  weatherBlock.innerHTML = template;
+}
+
+loadWeather();
